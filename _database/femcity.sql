c6347887771291304662c57e92d44aca71328ee1
Allan Jeremy <aj.dev254@gmail.com>
2017-03-15 23:56:34 +0300

    Added database structure file

---
+++ _database/femcity.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2017 at 09:55 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `femcity`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_accounts`
+--
+
+CREATE TABLE `admin_accounts` (
+  `acc_id` int(255) UNSIGNED NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `business_name` varchar(256) NOT NULL,
+  `cat_id` int(255) UNSIGNED NOT NULL COMMENT 'Category id',
+  `email` varchar(256) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `subbed` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_activated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_expires` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(255) UNSIGNED NOT NULL,
+  `cat_name` varchar(256) NOT NULL,
+  `cat_description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Categories for the various business types';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `featured_items`
+--
+
+CREATE TABLE `featured_items` (
+  `feature_id` int(255) UNSIGNED NOT NULL COMMENT 'Featured item id',
+  `item_id` int(255) UNSIGNED NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains the items featured in the home page';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `item_id` int(255) UNSIGNED NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `type` enum('service','product','','') NOT NULL DEFAULT 'service',
+  `description` text,
+  `price` int(255) UNSIGNED NOT NULL DEFAULT '10',
+  `quantity` int(10) NOT NULL DEFAULT '1',
+  `discount` int(255) UNSIGNED NOT NULL DEFAULT '0',
+  `acc_id` int(255) UNSIGNED NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Products or services provided by business owners';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promotions`
+--
+
+CREATE TABLE `promotions` (
+  `promo_id` int(11) NOT NULL,
+  `promo_text` varchar(512) NOT NULL,
+  `promo_description` text,
+  `item_id` int(255) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Contains promotions currently being displayed on banners';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superuser_accounts`
+--
+
+CREATE TABLE `superuser_accounts` (
+  `acc_id` int(255) UNSIGNED NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `subbed` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_accounts`
+--
+ALTER TABLE `admin_accounts`
+  ADD PRIMARY KEY (`acc_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `featured_items`
+--
+ALTER TABLE `featured_items`
+  ADD PRIMARY KEY (`feature_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `promotions`
+--
+ALTER TABLE `promotions`
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `superuser_accounts`
+--
+ALTER TABLE `superuser_accounts`
+  ADD PRIMARY KEY (`acc_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_accounts`
+--
+ALTER TABLE `admin_accounts`
+  MODIFY `acc_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `featured_items`
+--
+ALTER TABLE `featured_items`
+  MODIFY `feature_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Featured item id';
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `item_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `superuser_accounts`
+--
+ALTER TABLE `superuser_accounts`
+  MODIFY `acc_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin_accounts`
+--
+ALTER TABLE `admin_accounts`
+  ADD CONSTRAINT `fk_admin_acc_category` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`cat_id`);
+
+--
+-- Constraints for table `featured_items`
+--
+ALTER TABLE `featured_items`
+  ADD CONSTRAINT `fk_featured_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `promotions`
+--
+ALTER TABLE `promotions`
+  ADD CONSTRAINT `fk_promo_item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
